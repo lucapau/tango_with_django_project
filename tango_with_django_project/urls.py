2361f8589d0956eed614325dbe390a3c083de4f3
“Luca <2621045p@student.gla.ac.uk>
2024-01-19 15:16:12 +0000

    first commit

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -13,13 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from rango import views
+
 urlpatterns = [
-path('', views.index, name='index'),
-path('rango/', include('rango.urls')),
-# The above maps any URLs starting with rango/ to be handled by rango.
-path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('rango/', include('rango.urls')),
+    path('admin/', admin.site.urls),
 ]
+
+# Add the following line to serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
