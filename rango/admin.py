3bdc1a5ba48616e45704a66bc0690a29cbee5248
“Luca <2621045p@student.gla.ac.uk>
2024-01-26 16:24:09 +0000

    chapter 6

--- rango/admin.py
+++ rango/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from rango.models import Category, Page
-admin.site.register(Category)
+
 
 
 class PageAdmin(admin.ModelAdmin):
@@ -9,3 +9,11 @@ class PageAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Page, PageAdmin)
+
+
+# Add in this class to customize the Admin Interface
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+# Update the registration to include this customized interface
+admin.site.register(Category, CategoryAdmin)
