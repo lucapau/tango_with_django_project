5dfeede047bcf5742c091f59afee3fa0fe47c33d
“Luca <2621045p@student.gla.ac.uk>
2024-01-26 09:16:11 +0000

    chapter 5

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from rango.models import Category, Page
+admin.site.register(Category)
 
-# Register your models here.
+
+class PageAdmin(admin.ModelAdmin):
+    
+    list_display = ('title', 'category', 'url')
+
+
+admin.site.register(Page, PageAdmin)
