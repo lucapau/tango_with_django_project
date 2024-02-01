b6adc4534319e30ad1bd84b32d77b5d110b8f91e
“Luca <2621045p@student.gla.ac.uk>
2024-02-01 10:51:06 +0000

    chapter 7

--- rango/urls.py
+++ rango/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from rango import views
-app_name = 'rango'
-urlpatterns = [
-path('', views.index, name='index'),
-path('about/', views.about, name='about'),
-path('category/<slug:category_name_slug>/',
-views.show_category, name='show_category'),
 
+app_name = 'rango'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),
+    path('add_category/', views.add_category, name='add_category'),
+    # New URL pattern for the add_page view
+    path('category/<slug:category_name_slug>/add_page/', views.add_page, name='add_page'),
 ]
