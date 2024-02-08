a924ac5a7c430e3e645327a4ade039600d46d7e8
“Luca <2621045p@student.gla.ac.uk>
2024-02-08 10:59:52 +0000

    chapter 10

--- rango/views.py
+++ rango/views.py
@@ -10,6 +10,7 @@ from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from datetime import datetime
 
 
 
@@ -50,38 +51,28 @@ def show_category(request, category_name_slug):
 
 
 def index(request):
-    # Query the database for a list of ALL categories currently stored.
-    # Order the categories by the number of likes in descending order.
-    # Retrieve the top 5 only -- or all if less than 5.
     category_list = Category.objects.order_by('-likes')[:5]
-
-    # Query for top viewed pages
-    most_viewed_pages = Page.objects.order_by('-views')[:5]
-
-    # Prepare a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage matches to {{ boldmessage }} in the template!
+    page_list = Page.objects.order_by('-views')[:5]
     context_dict = {
-    'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!',
-    'categories': category_list,
-    'pages': most_viewed_pages  # Change 'top_pages' to 'pages'
-}
-
-
-
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
+        'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!',
+        'categories': category_list,
+        'pages': page_list,
+    }
+    
+    visitor_cookie_handler(request)
+    
     return render(request, 'rango/index.html', context=context_dict)
 
 
 
 
 def about(request):
+    visitor_cookie_handler(request)  
+    visits = request.session.get('visits', 1)  
     
-    context_dict = {'boldmessage': 'This tutorial has been put together by Luca Pau '} 
-
-    
+    context_dict = {'visits': visits}
     return render(request, 'rango/about.html', context=context_dict)
+
 @login_required
 def add_category(request):
     form = CategoryForm()
@@ -239,3 +230,28 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return redirect('rango:index')
+
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+# Updated the function definition
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+    
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        # Update the last visit cookie now that we have updated the count
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        # Set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
+    
+    # Update/set the visits cookie
+    request.session['visits'] = visits
